9482c531e39bc557b85f69e2ade338fd7639b6b0
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-04-28 11:19:41 +0100

    Initial Commit

---
+++ using-spring-jdbc-template/src/main/java/io/winnie/usingspringjdbctemplate/UsingSpringJdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package io.winnie.usingspringjdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingSpringJdbcTemplateApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(UsingSpringJdbcTemplateApplication.class, args);
+    }
+    
+}
